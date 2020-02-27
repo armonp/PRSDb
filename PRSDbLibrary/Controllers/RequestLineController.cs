58ff734d3830b971d5dc7d98d7954948a2cd9cb7
Armon Porter <max-student@maxtrain.com>
2020-02-27 12:16:54 -0500

    Added Requestline Controller

---
+++ PRSDbLibrary/Controllers/RequestLineController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using PRSDbLibrary.Models;
+
+namespace PRSDbLibrary.Controllers {
+    public class RequestLineController {
+        private AppDbContext context = new AppDbContext();
+
+        public IEnumerable<RequestLine> GetAllReqLines() {
+            return context.RequestLines.ToList();
+        }
+        public RequestLine GetReqLineById(int id) {
+            if (id < 1) throw new Exception("Id must be greater than zero");
+            var rl = context.RequestLines.Find(id);
+            if ( rl == null) throw new Exception("RequestLine Not Found");
+            return rl;
+        }
+        public RequestLine AddRequestLine(RequestLine reqline) {
+            if (reqline == null) throw new Exception("RequestLine cannot be null");
+            context.RequestLines.Add(reqline);
+            context.SaveChanges();
+            Console.WriteLine($"Line added to {reqline.Request} for {reqline.Qty} {reqline.Product} added successfully !");
+            return reqline;
+        }
+        public bool UpdateRequestLine(int id, RequestLine reqline) {
+            if (RequestLine == null) throw new Exception("RequestLine cannot be null");
+            if (id != RequestLine.Id) throw new Exception("Id must be same as RequestLine.Id");
+            context.Entry(RequestLine).State = EntityState.Modified;
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool DeleteRequestLine(int id) {
+            var rl = GetReqLineById(id);
+            return DeleteRequestLine(rl);
+        }
+        public bool DeleteRequestLine(RequestLine reqline) {
+            context.RequestLines.Remove(reqline);
+            var recs = context.SaveChanges();
+            if (recs != 1) throw new Exception("Delete failed");
+            else Console.WriteLine("Delete successful");
+            return true;
+        }
+    }
+}
--- PRSDbLibrary/Models/RequestLine.cs
+++ PRSDbLibrary/Models/RequestLine.cs
@@ -7,7 +7,6 @@ namespace PRSDbLibrary.Models {
         public int RequestId { get; set; }
         public int ProductId { get; set; }
         [Required] public int Qty { get; set; } = 1;
-
         public virtual Product Product{ get; set;}
         public virtual Request Request { get; set; }
 
