5f9d631641993a3ca74a12abd32f81ce50a806ff
Armon Porter <max-student@maxtrain.com>
2020-02-27 10:57:57 -0500

    Updated Usercontroller

--- PRSDb/Program.cs
+++ PRSDb/Program.cs
@@ -12,26 +12,11 @@ namespace PRSDb {
             //UserCtrl.AddUser("admin", "admin1", "Armon", "Porter", "9375130000", "email@email.com", true, true);
             //UserCtrl.AddUser("test", "test", "test", "this", "isgoing", "tobedeleted", false, false);
             //UserCtrl.DeleteUser(2);
-            UserCtrl.GetUserByPk(1).ToString();
-            Console.WriteLine();
+            UserCtrl.GetUserByPk(1);
             //UserCtrl.GetAllUsers();
-            Console.WriteLine();
          
 
         }
-        //static string Login(AppDbContext context, string username, string password) {
-        //    var success = context.Users.Single(c => c.Username == username && c.Password == password);
-        //    if (success == null)
-        //        return ("Login Failed");
-        //    else
-        //        return ($"Welcome {success.Firstname}!");
-        //}
-        
 
-        //Add Product
-        //Update User
-        //Delete User
-
-        //Add
     }
 }
--- PRSDbLibrary/Controllers/UserController.cs
+++ PRSDbLibrary/Controllers/UserController.cs
@@ -3,15 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace PRSDbLibrary.Controllers {
     public class UserController {
         private AppDbContext context { get; set; } = new AppDbContext();
         
-           
         //GetAllUsers
-        public List<User> GetAllUsers() {
-            //var context = new AppDbContext();
+        public IEnumerable<User> GetAllUsers() { 
             var users = context.Users.ToList();
             foreach (var u in users) {
                 Console.WriteLine($"{u.Firstname} | {u.Lastname} | {u.Username} | {u.Phone} | {u.Email} | {u.IsAdmin} | {u.IsReviewer} ");
@@ -20,40 +19,62 @@ namespace PRSDbLibrary.Controllers {
         }
         //GetOneUser
         public User GetUserByPk(int id) {
+            if (id < 1) throw new Exception("Id must be greater than zero");
             var user = context.Users.Find(id);
             if (user == null) throw new Exception("User Not Found");
-            else return user;
-            //Console.WriteLine($"{user.Firstname} | {user.Lastname} | {user.Username} | {user.Phone} | {user.Email} | {user.IsAdmin} | {user.IsReviewer}");
+            else Console.WriteLine($"{user.Firstname} | {user.Lastname} | {user.Username} | {user.Phone} | {user.Email} | {user.IsAdmin} | {user.IsReviewer}");
+            return user;
         }
 
         //Add User 
-        public void AddUser(string username, string password, string firstname, string lastname, string phone, string email, bool reviewer, bool admin) {
-            var user = new User {
-                Id = 0, Username = username,
-                Password = password, Firstname = firstname,
-                Lastname = lastname, Phone = phone,
-                Email = email, IsAdmin = false, IsReviewer = false
-            };
+        public User AddUser(User user) { //will need to define new user in program class to be added
+            if (user == null) throw new Exception("User cannot be null"); //classes can be null so need to check that null User isn't being passed
             context.Users.Add(user);
-            var recs = context.SaveChanges();
-            if (recs != 1) throw new Exception("Add failed"); else Console.WriteLine($"{user.Firstname} added successfully !");
+            try {
+                context.SaveChanges();
+            } catch(DbUpdateException ex) {
+                throw new Exception("Username must be unique", ex);
+            } catch (Exception ex) {
+                throw;
+            }
+            Console.WriteLine($"{user.Firstname} added successfully !");
+            return user;
         }
         //Update user
-        public void UpdateUser(int id, string username, string password, string firstname, string lastname, string phone, string email, bool reviewer, bool admin) {
-            var user = GetUserByPk(id);
-            user.Firstname = firstname; user.Username = username;
-            user.Lastname = lastname; user.Password = password;
-            user.Phone = phone; user.Email = email;
-            user.IsReviewer = reviewer; user.IsAdmin = admin;
+        public bool UpdateUser(int id, User user) {
+            if (user == null) throw new Exception("User cannot be null");
+            if (id != user.Id) throw new Exception("Id and User.Id must match");
+            context.Entry(user).State = EntityState.Modified; //alternate to finding user by id then changing each individual property
+            try {
+                context.SaveChanges();
+            } catch (DbUpdateException ex) {
+                throw new Exception("Username must be unique", ex);
+            } catch (Exception ex) {
+                throw;
+            }
+            return true;
         }
         //Delete User
-        public void DeleteUser(int id) {
+        public bool DeleteUser(int id) {
+            if (id <= 0) throw new Exception("ID must be greater than zero");
             var user = context.Users.Find(id);
             if (user == null) throw new Exception("User not found");
+            return Delete(user);
+        }
+        public bool Delete(User user) {
             context.Users.Remove(user);
             var recs = context.SaveChanges();
             if (recs != 1) throw new Exception("Delete failed");
             else Console.WriteLine("Delete successful");
+            return true;
+        }
+        //Login
+        public string Login(string username, string password) {
+            var success = context.Users.Single(c => c.Username == username && c.Password == password);
+            if (success == null)
+                return ("Login Failed");
+            else
+                return ($"Welcome {success.Firstname}!");
         }
 
     }
